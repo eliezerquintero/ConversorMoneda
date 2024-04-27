362fff24d56daaf28d251892efb446fee74d3015
eliezerquintero <eliezerquintero2@gmail.com>
2024-04-26 19:05:29 -0600

    Invocacion de API

---
+++ src/Conversion.java
@@ -0,0 +1,13 @@
+public class Conversion {
+    String monedaOrigne;
+    String monedaDestino;
+    public Conversion(String monedaOrigne, String monedaDestino){
+        this.monedaOrigne = monedaOrigne;
+        this.monedaDestino = monedaDestino;
+    }
+    public double RealizaroConversion(double cantidad){
+        double tasaConversion = 0;
+        cantidad
+
+    }
+}
